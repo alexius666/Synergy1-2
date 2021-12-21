5c416a9fe7da345bce005d2a282c9ae72e583eeb
Alexius <alexius666@yandex.ru>
2021-12-21 18:21:28 +0500

    Upd:IJ (square & perimeter)

--- Lesson1-2.java
+++ Lesson1-2.java
@@ -1,6 +1,12 @@
 class Main {
     public static void main(String[] args) {
-    int a=1;
+        int width = 16; // ширина прямойгольника
+        int height = 21; // высота прямоугольника
+        int square = width * height; // вычисление площади прямоугольника
+        int perimeter = 2 * width + 2 * height; // // вычисление перимерта прямоугольника
+
+        System.out.println("Площадь прямоугольника равна "+square);
+        System.out.println("Периметр прямоугольника равен "+perimeter);
     }
   
 }  
